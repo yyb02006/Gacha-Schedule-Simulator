a35a196bd7a4569cfeb07d9f131760c25d5a57a8
alwjrqns <yyb02006@gmail.com>
2025-09-17 23:18:54 +0900

    Refactor : main 그룹라우트 추가

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,10 @@
+import Header from '#/components/header/Header';
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <div>{children}</div>
+    </>
+  );
+}
---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import PickupList from '#/components/PickupList';
+
+export default function Home() {
+  return (
+    <section className="flex h-screen w-screen flex-col items-center">
+      <PickupList />
+    </section>
+  );
+}
