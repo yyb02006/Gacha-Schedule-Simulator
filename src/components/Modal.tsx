97459641038ba6fec908ed46494ea100fe8add3d
alwjrqns <yyb02006@gmail.com>
2025-09-21 21:39:43 +0900

    Feat : 모달에서 모달 헤더 제거

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,53 +3,12 @@
 import { ReactNode, useEffect, useState } from 'react';
 import { AnimatePresence, motion } from 'motion/react';
 import { createPortal } from 'react-dom';
-import { cancelButtonVariants, toOpacityZero } from '#/constants/variants';
 
 interface ModalProps {
   children: ReactNode;
   isOpen: boolean;
   onClose: () => void;
 }
-
-const CancelButton = ({
-  handleCancel,
-  isFirstRenderOver = true,
-}: {
-  handleCancel: () => void;
-  isFirstRenderOver?: boolean;
-}) => {
-  const [isHover, setIsHover] = useState(false);
-  return (
-    <motion.button
-      key="cancel"
-      onHoverStart={() => {
-        setIsHover(true);
-      }}
-      onHoverEnd={() => {
-        setIsHover(false);
-      }}
-      onClick={handleCancel}
-      variants={cancelButtonVariants}
-      viewport={{ once: true, amount: 0.5 }}
-      custom={{ state: isFirstRenderOver ? 'normal' : 'initial' }}
-      initial="exit"
-      animate={isHover ? 'hover' : 'idle'}
-      exit="exit"
-      className="size-[44px] cursor-pointer rounded-xl p-2 text-[#ff637e]"
-    >
-      <motion.svg
-        variants={toOpacityZero}
-        whileInView="idle"
-        viewport={{ once: true, amount: 0.5 }}
-        initial="exit"
-        exit="exit"
-        className="size-full"
-      >
-        <use href="/icons/icons.svg#cancel" />
-      </motion.svg>
-    </motion.button>
-  );
-};
 
 export default function Modal({ children, isOpen, onClose }: ModalProps) {
   const [mounted, setMounted] = useState(false);
@@ -84,24 +43,16 @@ export default function Modal({ children, isOpen, onClose }: ModalProps) {
             }}
             className="fixed top-0 left-0 flex h-screen w-screen items-center justify-center bg-[#00000090]"
           >
-            <motion.div
-              animate={{ scaleY: 1, transition: { duration: 0.3, type: 'spring' } }}
-              initial={{ scaleY: 0 }}
-              exit={{ scaleY: 0, transition: { duration: 0.3, type: 'spring' } }}
-              className="size-full max-h-[640px] max-w-[960px] rounded-xl bg-[#202020] p-6"
-            >
-              <div className="flex items-center justify-between">
-                <h1 className="font-S-CoreDream-700 text-2xl">
-                  <span className="text-amber-400">고오급</span> 옵션
-                </h1>
-                <CancelButton
-                  handleCancel={() => {
-                    onClose();
-                  }}
-                />
-              </div>
-              {children}
-            </motion.div>
+            <div className="h-screen py-6">
+              <motion.div
+                animate={{ scaleY: 1, transition: { duration: 0.3, type: 'spring' } }}
+                initial={{ scaleY: 0 }}
+                exit={{ scaleY: 0, transition: { duration: 0.3, type: 'spring' } }}
+                className="flex h-full max-w-[960px] flex-col space-y-6 rounded-xl bg-[#202020] p-6"
+              >
+                {children}
+              </motion.div>
+            </div>
           </motion.div>
         )}
       </AnimatePresence>,
