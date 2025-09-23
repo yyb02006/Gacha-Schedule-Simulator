23b888b1e30dd6c376b16512684f48e990d18b2a
alwjrqns <yyb02006@gmail.com>
2025-09-24 06:54:24 +0900

    Feat : id => type

--- src/constants/ui.ts
+++ src/constants/ui.ts
@@ -3,17 +3,17 @@ import { GachaTypeButton } from '#/types/types';
 export const gachaTypeButtons: GachaTypeButton[] = [
   {
     name: '한정',
-    id: 'limited',
+    type: 'limited',
     hoverBackground: 'linear-gradient(155deg, #bb4d00, #ffb900)',
   },
   {
     name: '일반',
-    id: 'standard',
+    type: 'standard',
     hoverBackground: 'linear-gradient(155deg, #1447e6, #51a2ff)',
   },
   {
     name: '콜라보',
-    id: 'collab',
+    type: 'collab',
     hoverBackground: 'linear-gradient(155deg, #ec003f, #ff637e)',
   },
 ];
