1fe28e5914997345155f491128d7341adf8235f5
alwjrqns <yyb02006@gmail.com>
2025-09-26 23:42:54 +0900

    Feat : 유틸함수 추가

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -13,3 +13,60 @@
 export function cls(...className: string[]) {
   return className.join(' ');
 }
+
+/**
+ *
+ * 주어진 문자열을 정수로 변환하여 반환하되 빈문자열일 경우 0으로 반환
+ *
+ * @param {string} string - 변환할 문자열
+ * @returns {number} 변환된 숫자
+ *
+ * @example
+ * const resultNumber = stringToNumber('5');
+ * console.log(resultNumber); // 5
+ *
+ * @example
+ * const resultNumber = stringToNumber('');
+ * console.log(resultNumber); // 0
+ *
+ * @example
+ * const resultNumber = stringToNumber('abc');
+ * console.log(resultNumber); // NaN
+ */
+export function stringToNumber(string: string) {
+  return string === '' ? 0 : parseInt(string);
+}
+
+/**
+ * 주어진 문자열이 숫자로 이루어져 있는지 판단하고, 선행 0을 제거한 후 반환
+ * 빈 문자열일 경우 '0'으로 반환하거나 빈 문자열을 반환
+ *
+ * @param {string} string - 변환할 문자열
+ * @param {boolean} [returnEmptyForZero=true] - 빈 문자열일 경우 빈 문자열을 반환할지 여부
+ * @returns {string | undefined} 변환된 문자열 또는 숫자가 아닌 경우 undefined
+ *
+ * @example
+ * const resultNumber = normalizeNumberString('5');
+ * console.log(resultNumber); // '5'
+ *
+ * @example
+ * const resultNumber = normalizeNumberString('05');
+ * console.log(resultNumber); // '5'
+ *
+ * @example
+ * const resultNumber = normalizeNumberString('');
+ * console.log(resultNumber); // '0'
+ *
+ * @example
+ * const resultNumber = normalizeNumberString('', false);
+ * console.log(resultNumber); // ''
+ *
+ * @example
+ * const resultNumber = normalizeNumberString('abc');
+ * console.log(resultNumber); // undefined
+ */
+export function normalizeNumberString(string: string, returnEmptyForZero: boolean = true) {
+  if (isNaN(Number(string))) return undefined;
+  const trimmedLeadingZero = string.replace(/^0+/, '');
+  return trimmedLeadingZero === '' && returnEmptyForZero ? '0' : trimmedLeadingZero;
+}
