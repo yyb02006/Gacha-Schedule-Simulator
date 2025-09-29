27d71e6b080b0f586c5555b8be62af2ab86b221f
alwjrqns <yyb02006@gmail.com>
2025-09-30 05:37:31 +0900

    Feat : 4성 뱃지 추가 및 로직 수정

--- src/components/modals/OperatorBadgeEditModal.tsx
+++ src/components/modals/OperatorBadgeEditModal.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import Badge from '#/components/badge';
+import Badge from '#/components/Badge';
 import CancelButton from '#/components/buttons/CancelButton';
 import TypeSelectionButton from '#/components/buttons/TypeSelectionButton';
 import Modal from '#/components/modals/Modal';
-import { OperatorRarity, OperatorType } from '#/components/PickupList';
+import { OperatorRarity, OperatorType, rarities } from '#/components/PickupList';
 import { operatorBadgeProps } from '#/constants/ui';
 import { toOpacityZero } from '#/constants/variants';
 import { motion } from 'motion/react';
@@ -25,22 +25,26 @@ export default function OperatorBadgeEditModal({
 }) {
   const {
     operatorType: { limited, normal },
-    rarity: { fifth, sixth },
   } = operatorBadgeProps;
-  const [currentBadges, setCurrentBadges] = useState<{
+  const [currentState, setCurrentState] = useState<{
     operatorType: OperatorType;
     rarity: OperatorRarity;
   }>({
     operatorType,
     rarity,
   });
+  const selectedRarityBadge = operatorBadgeProps.rarity[rarities[currentState.rarity]];
+  const unSelectedRarityBadges = Object.values(operatorBadgeProps.rarity).filter(
+    (badgeProp) => badgeProp.id !== currentState.rarity,
+  );
+  const isLimited = currentState.operatorType === 'limited';
   const onEditConfirmClick = () => {
     onClose();
-    onBadgeEdit(currentBadges.operatorType, currentBadges.rarity);
+    onBadgeEdit(currentState.operatorType, currentState.rarity);
   };
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="flex w-[360px] flex-col gap-y-8">
+      <div className="flex w-[360px] flex-col gap-y-6">
         <div className="flex items-center justify-between gap-x-6">
           <motion.h1
             variants={toOpacityZero}
@@ -53,71 +57,91 @@ export default function OperatorBadgeEditModal({
           </motion.h1>
           <CancelButton handleCancel={onClose} />
         </div>
-        <div className="flex gap-x-2">
-          {currentBadges.operatorType === 'limited' ? (
-            <Badge
-              key={'normal'}
-              {...normal.props}
-              animation={true}
-              onBadgeClick={() => {
-                setCurrentBadges((p) => ({ ...p, operatorType: 'normal' }));
-              }}
-              isLayout
-            />
-          ) : (
-            <Badge
-              key={'limited'}
-              color={
-                currentBadges.rarity === 5 ? 'border-[#606060] text-[#606060]' : limited.props.color
-              }
-              name={limited.props.name}
-              animation={currentBadges.rarity === 5 ? false : true}
-              onBadgeClick={() => {
-                if (currentBadges.rarity === 5) return;
-                setCurrentBadges((p) => ({ ...p, operatorType: 'limited' }));
-              }}
-              isLayout
-            />
-          )}
-          {currentBadges.rarity === 6 ? (
-            <Badge
-              key={'fifth'}
-              color={
-                currentBadges.operatorType === 'limited'
-                  ? 'border-[#606060] text-[#606060]'
-                  : fifth.props.color
-              }
-              name={fifth.props.name}
-              animation={currentBadges.operatorType === 'limited' ? false : true}
-              onBadgeClick={() => {
-                if (currentBadges.operatorType === 'limited') return;
-                setCurrentBadges((p) => ({ ...p, rarity: 5 }));
-              }}
-              isLayout
-            />
-          ) : (
-            <Badge
-              key={'sixth'}
-              {...sixth.props}
-              animation={true}
-              onBadgeClick={() => {
-                setCurrentBadges((p) => ({ ...p, rarity: 6 }));
-              }}
-              isLayout
-            />
-          )}
+        <div className="space-y-2">
+          <motion.div
+            variants={toOpacityZero}
+            initial="exit"
+            animate="idle"
+            exit="exit"
+            className="text-sm"
+          >
+            <span className="text-sky-500">미선택</span> 뱃지
+          </motion.div>
+          <div className="flex gap-x-2">
+            {currentState.operatorType === 'limited' ? (
+              <Badge
+                key={'normal'}
+                {...normal.props}
+                animation={true}
+                onBadgeClick={() => {
+                  setCurrentState((p) => ({ ...p, operatorType: 'normal' }));
+                }}
+                isLayout
+              />
+            ) : (
+              <Badge
+                key={'limited'}
+                color={
+                  currentState.rarity !== 6
+                    ? 'border-[#606060] text-[#606060]'
+                    : limited.props.color
+                }
+                name={limited.props.name}
+                animation={currentState.rarity === 6 ? true : false}
+                onBadgeClick={() => {
+                  if (currentState.rarity !== 6) return;
+                  setCurrentState((p) => ({ ...p, operatorType: 'limited' }));
+                }}
+                isLayout
+              />
+            )}
+            {unSelectedRarityBadges.map((filteredBadgeProp) => {
+              const isSixth = filteredBadgeProp.id === 6;
+              return (
+                <Badge
+                  key={rarities[filteredBadgeProp.id]}
+                  color={
+                    isLimited && !isSixth
+                      ? 'border-[#606060] text-[#606060]'
+                      : filteredBadgeProp.props.color
+                  }
+                  name={filteredBadgeProp.props.name}
+                  animation={isLimited && !isSixth ? false : true}
+                  onBadgeClick={() => {
+                    if (isLimited && !isSixth) return;
+                    setCurrentState((p) => ({ ...p, rarity: filteredBadgeProp.id }));
+                  }}
+                  isLayout
+                />
+              );
+            })}
+          </div>
         </div>
-        <div className="flex gap-x-2">
-          {currentBadges.operatorType === 'limited' ? (
-            <Badge key={'limited'} {...limited.props} isLayout />
-          ) : (
-            <Badge key={'normal'} {...normal.props} isLayout />
-          )}
-          {currentBadges.rarity === 6 ? (
-            <Badge key={'sixth'} {...sixth.props} isLayout />
-          ) : (
-            <Badge key={'fifth'} {...fifth.props} isLayout />
-          )}
+        <div className="space-y-2">
+          <motion.div
+            variants={toOpacityZero}
+            initial="exit"
+            animate="idle"
+            exit="exit"
+            className="text-sm"
+          >
+            <span className="text-amber-400">선택된</span> 뱃지
+          </motion.div>
+          <div className="flex gap-x-2">
+            {currentState.operatorType === 'limited' ? (
+              <Badge key={'limited'} {...limited.props} isLayout />
+            ) : (
+              <Badge key={'normal'} {...normal.props} isLayout />
+            )}
+            {selectedRarityBadge ? (
+              <Badge
+                key={rarities[selectedRarityBadge.id]}
+                color={selectedRarityBadge.props.color}
+                name={selectedRarityBadge.props.name}
+                isLayout
+              />
+            ) : null}
+          </div>
         </div>
         <TypeSelectionButton
           name="수정완료"
