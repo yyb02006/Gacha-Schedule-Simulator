3fd63692ab0fd0ac4a68701838a74e423317c4b2
alwjrqns <yyb02006@gmail.com>
2025-10-06 23:58:09 +0900

    Feat : 마우스다운 애니메이션 추가

--- src/components/buttons/CancelButton.tsx
+++ src/components/buttons/CancelButton.tsx
@@ -10,21 +10,31 @@ export default function CancelButton({
   isFirstRenderOver?: boolean;
 }) {
   const [isHover, setIsHover] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
   return (
     <motion.button
       key="cancel"
+      onClick={handleCancel}
       onHoverStart={() => {
         setIsHover(true);
       }}
       onHoverEnd={() => {
         setIsHover(false);
       }}
-      onClick={handleCancel}
+      onMouseDown={() => setIsClicked(true)}
+      onMouseUp={() => setIsClicked(false)}
+      onMouseLeave={() => setIsClicked(false)}
+      onTapStart={() => setIsClicked(true)}
+      onTapCancel={() => setIsClicked(false)}
       variants={cancelButtonVariants}
       viewport={{ once: true, amount: 0.5 }}
-      custom={{ state: isFirstRenderOver ? 'normal' : 'initial' }}
+      custom={{
+        state: isFirstRenderOver ? 'normal' : 'initial',
+        background: 'linear-gradient(135deg, #bd1b36, #ff637e)',
+        color: '#eaeaea',
+      }}
       initial="exit"
-      animate={isHover ? 'hover' : 'idle'}
+      animate={isClicked ? 'active' : isHover ? 'hover' : 'idle'}
       exit="exit"
       className="size-[44px] cursor-pointer rounded-xl p-2 text-[#ff637e]"
     >
