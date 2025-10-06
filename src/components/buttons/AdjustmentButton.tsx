5f4d116ffdca2bbd61cad35c93f6b3cf143f22b9
alwjrqns <yyb02006@gmail.com>
2025-10-06 23:57:55 +0900

    Feat : 마우스다운 애니메이션 추가

--- src/components/buttons/AdjustmentButton.tsx
+++ src/components/buttons/AdjustmentButton.tsx
@@ -4,61 +4,74 @@ import { optionButtonVariants, toOpacityZero } from '#/constants/variants';
 
 export default function AdjustmentButton({ onClick }: { onClick: () => void }) {
   const [isHover, setIsHover] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
   const [isFirstRenderOver, setIsFirstRenderOver] = useState(false);
   return (
     <motion.button
+      onClick={onClick}
       onHoverStart={() => setIsHover(true)}
       onHoverEnd={() => setIsHover(false)}
-      onViewportEnter={() => setIsFirstRenderOver(true)}
-      onClick={onClick}
-      variants={optionButtonVariants}
-      whileInView="idle"
-      whileHover="hover"
-      viewport={{ once: true, amount: 0.5 }}
-      custom={{ state: isFirstRenderOver ? 'normal' : 'initial' }}
-      initial="exit"
-      exit="exit"
-      className="relative flex size-[44px] cursor-pointer items-center justify-between self-end rounded-xl p-2"
+      onMouseDown={() => setIsClicked(true)}
+      onMouseUp={() => setIsClicked(false)}
+      onMouseLeave={() => setIsClicked(false)}
+      onTapStart={() => setIsClicked(true)}
+      onTapCancel={() => setIsClicked(false)}
+      className="rounded-xl"
     >
-      <div className="absolute top-0 left-0 flex size-full flex-col justify-between px-2 py-3">
-        {[
-          [12, 2, 17, 12],
-          [5, 21, 15, 5],
-          [17, 10, 1, 17],
-        ].map((sequence, index) => (
-          <motion.div
-            key={index}
-            variants={toOpacityZero}
-            whileInView="idle"
-            viewport={{ once: true, amount: 0.5 }}
-            initial="exit"
-            animate={isHover ? { background: '#ffffff' } : { background: '#ffb900' }}
-            className="flex h-[2px] items-center rounded-full bg-[#ffb900]"
-          >
+      <motion.div
+        onViewportEnter={() => setIsFirstRenderOver(true)}
+        variants={optionButtonVariants}
+        viewport={{ once: true, amount: 0.5 }}
+        custom={{
+          state: isFirstRenderOver ? 'normal' : 'initial',
+          background: 'linear-gradient(135deg, #bb4d00, #ffb900)',
+          color: '#eaeaea',
+        }}
+        animate={isClicked ? 'active' : isHover ? 'hover' : 'idle'}
+        initial="exit"
+        exit="exit"
+        className="relative flex size-[44px] cursor-pointer items-center justify-between self-end rounded-xl p-2"
+      >
+        <div className="absolute top-0 left-0 flex size-full flex-col justify-between px-2 py-3">
+          {[
+            [12, 2, 17, 12],
+            [5, 21, 15, 5],
+            [17, 10, 1, 17],
+          ].map((sequence, index) => (
             <motion.div
+              key={index}
               variants={toOpacityZero}
               whileInView="idle"
               viewport={{ once: true, amount: 0.5 }}
-              initial={{ ...toOpacityZero.exit, x: sequence[0] }}
-              animate={
-                isHover
-                  ? {
-                      backgroundColor: '#ffffff',
-                      x: sequence,
-                      transition: {
-                        x: {
-                          duration: 3,
-                          repeat: Infinity,
+              initial="exit"
+              animate={isHover ? { background: '#ffffff' } : { background: '#ffb900' }}
+              className="flex h-[2px] items-center rounded-full bg-[#ffb900]"
+            >
+              <motion.div
+                variants={toOpacityZero}
+                whileInView="idle"
+                viewport={{ once: true, amount: 0.5 }}
+                initial={{ ...toOpacityZero.exit, x: sequence[0] }}
+                animate={
+                  isHover
+                    ? {
+                        backgroundColor: '#ffffff',
+                        x: sequence,
+                        transition: {
+                          x: {
+                            duration: 3,
+                            repeat: Infinity,
+                          },
                         },
-                      },
-                    }
-                  : { x: sequence[0] }
-              }
-              className="size-2 rounded-full bg-amber-400"
-            />
-          </motion.div>
-        ))}
-      </div>
+                      }
+                    : { x: sequence[0] }
+                }
+                className="size-2 rounded-full bg-amber-400"
+              />
+            </motion.div>
+          ))}
+        </div>
+      </motion.div>
     </motion.button>
   );
 }
