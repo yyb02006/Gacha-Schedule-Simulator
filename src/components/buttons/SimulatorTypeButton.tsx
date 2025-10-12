cb769ac60ec840f9e827707c043497aded221518
alwjrqns <yyb02006@gmail.com>
2025-10-12 22:24:18 +0900

    Feat : 드래그 이벤트 받을 수 있도록 수정 및 리팩토링

--- src/components/buttons/SimulatorTypeButton.tsx
+++ src/components/buttons/SimulatorTypeButton.tsx
@@ -6,15 +6,26 @@ import {
   toggleButtonVariants,
   toOpacityZero,
 } from '#/constants/variants';
-import { motion } from 'motion/react';
+import { animate } from 'motion';
+import { motion, useMotionValue } from 'motion/react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function SimulatorTypeButton({
   isGachaSim,
   onTypeClick,
 }: {
   isGachaSim: boolean;
-  onTypeClick: () => void;
+  onTypeClick: (isLeft?: boolean) => void;
 }) {
+  const [isDragging, setIsDragging] = useState(false);
+  const constraintsRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  useEffect(() => {
+    const rect = constraintsRef.current?.getBoundingClientRect();
+    if (!rect) return;
+    const maxX = rect.width / 2;
+    animate(x, isGachaSim ? 0 : maxX, { type: 'spring', stiffness: 400, damping: 30 });
+  }, [isGachaSim]);
   return (
     <div className="flex min-w-[100px] flex-col space-y-1">
       <motion.div
@@ -23,7 +34,10 @@ export default function SimulatorTypeButton({
         viewport={{ once: true, amount: 0.5 }}
         initial="exit"
         exit="exit"
-        onClick={onTypeClick}
+        onClick={() => {
+          if (isDragging) return;
+          onTypeClick();
+        }}
         className="relative flex h-[48px] cursor-pointer items-center justify-center rounded-xl px-4 pt-3 pb-2 font-bold"
       >
         <motion.div
@@ -60,13 +74,36 @@ export default function SimulatorTypeButton({
             재화 소모 시뮬레이션
           </motion.div>
         </motion.div>
-        <div
-          style={{ justifyContent: isGachaSim ? 'flex-start' : 'flex-end' }}
-          className="absolute top-0 flex size-full justify-end"
-        >
+        <div ref={constraintsRef} className="absolute top-0 flex size-full">
           <motion.div
-            layout
-            transition={{ type: 'spring', visualDuration: 0.3, bounce: 0.2 }}
+            drag="x"
+            dragConstraints={constraintsRef}
+            dragElastic={0.04}
+            dragMomentum={false}
+            onDragStart={() => {
+              setIsDragging(true);
+            }}
+            onDragEnd={() => {
+              setIsDragging(false);
+              const rect = constraintsRef.current?.getBoundingClientRect();
+              if (!rect) return;
+              const maxX = rect.width / 2;
+              const currentX = x.get();
+              if (isGachaSim) {
+                if (currentX > maxX * (1 / 4)) {
+                  onTypeClick(false);
+                } else {
+                  animate(x, 0, { type: 'spring' });
+                }
+              } else {
+                if (currentX < maxX * (3 / 4)) {
+                  onTypeClick(true);
+                } else {
+                  animate(x, maxX, { type: 'spring' });
+                }
+              }
+            }}
+            style={{ x }}
             className="relative h-full w-1/2 p-[2px]"
           >
             <motion.div
