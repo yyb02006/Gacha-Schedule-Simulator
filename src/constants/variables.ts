41603f21b7b2d7b80e4e4cc8082c31d09c83c1fa
alwjrqns <yyb02006@gmail.com>
2025-10-25 23:39:30 +0900

    Feat : rarities 변수 variables로 이동

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -1,2 +1,10 @@
 export const rarityStrings = ['sixth', 'fifth', 'fourth'] as const;
 export const obtainedTypes = ['totalObtained', 'pickupObtained', 'targetObtained'] as const;
+export const rarities = {
+  6: 'sixth',
+  5: 'fifth',
+  4: 'fourth',
+  sixth: 6,
+  fifth: 5,
+  fourth: 4,
+} as const;
