e0249a31468c636087446f812ea397fc6b40b43c
alwjrqns <yyb02006@gmail.com>
2025-10-27 22:47:47 +0900

    Refactor : CustomTooltip을 외부에서 받도록 교체

--- src/components/charts/SummaryBrushBarChart.tsx
+++ src/components/charts/SummaryBrushBarChart.tsx
@@ -1,4 +1,3 @@
-import { truncateToTwoDecimals } from '#/libs/utils';
 import {
   Bar,
   BarChart,
@@ -11,36 +10,18 @@ import {
   YAxis,
 } from 'recharts';
 
-const CustomTooltip: React.FC<
-  TooltipContentProps<number, string> & { totalSimulationTry: number }
-> = ({ active, payload, label, totalSimulationTry }) => {
-  if (!active || !payload || payload.length === 0) return null;
-  return (
-    <div className="space-y-3 rounded-lg bg-[#202020] px-4 py-3 shadow-lg shadow-[#202020]">
-      <p className="text-lg">{label}</p>
-      {payload.map((data, index) => (
-        <div key={index} className="font-S-CoreDream-400 space-y-[2px] text-sm">
-          <p>
-            {`배너 성공 횟수 : `}
-            <span className="text-amber-400">{`${data.value} 회`}</span>
-          </p>
-          <p>
-            {`배너 성공률 : `}
-            <span className="text-amber-400">{`${truncateToTwoDecimals((data.value / totalSimulationTry) * 100)}%`}</span>
-          </p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
 export default function BrushBarChart({
   totalSimulationTry,
   data,
+  customTooltip,
+  yAxisTickFormatter,
 }: {
   totalSimulationTry: number;
   data: { name: string; value: number }[];
+  customTooltip?: React.FC<TooltipContentProps<number, string> & { totalSimulationTry: number }>;
+  yAxisTickFormatter?: (value: number, index: number) => string;
 }) {
+  const CustomTooltip = customTooltip;
   return (
     <BarChart
       style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
@@ -56,20 +37,18 @@ export default function BrushBarChart({
     >
       <CartesianGrid strokeDasharray="2 2" />
       <XAxis dataKey="name" className="text-xs" />
-      <YAxis
-        width="auto"
-        className="text-xs"
-        tickFormatter={(value) => `${((value / totalSimulationTry) * 100).toFixed(0)}%`}
-      />
+      <YAxis width="auto" className="text-xs" tickFormatter={yAxisTickFormatter} />
       <Tooltip
         content={
-          <CustomTooltip
-            payload={data}
-            totalSimulationTry={totalSimulationTry}
-            active
-            coordinate={{ x: 0, y: 0 }}
-            accessibilityLayer
-          />
+          CustomTooltip ? (
+            <CustomTooltip
+              payload={data}
+              totalSimulationTry={totalSimulationTry}
+              active
+              coordinate={{ x: 0, y: 0 }}
+              accessibilityLayer
+            />
+          ) : undefined
         }
         cursor={false}
       />
