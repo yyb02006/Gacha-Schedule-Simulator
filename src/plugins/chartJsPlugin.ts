704a28e9765aa0f1fbab574cdbd971220c682fec
alwjrqns <yyb02006@gmail.com>
2025-10-31 00:11:01 +0900

    Feat : 외부 라벨과 차트를 연결하는 라인 생성 플러그인 추가

---
+++ src/plugins/chartJsPlugin.ts
@@ -0,0 +1,36 @@
+import { ArcElement, Chart } from 'chart.js';
+
+export const doughnutConnectorPlugin = (total?: number) => ({
+  id: 'connectorLines',
+  afterDraw(chart: Chart<'doughnut'>) {
+    const ctx = chart.ctx;
+    const dataset = chart.data.datasets[0];
+    const totalizedValue = total
+      ? total
+      : (dataset.data as number[]).reduce((sum, val) => sum + val, 0);
+    dataset.data.forEach((value: number, index: number) => {
+      const ratio = value / totalizedValue;
+      if (ratio < 0.05 && ratio > 0) {
+        const meta = chart.getDatasetMeta(0);
+        const arc = meta.data[index] as ArcElement;
+        const angle = (arc.startAngle + arc.endAngle) / 2;
+        const radius = arc.outerRadius;
+        const x = arc.x + Math.cos(angle) * radius;
+        const y = arc.y + Math.sin(angle) * radius;
+        const labelX = arc.x + Math.cos(angle) * (radius + 8); // 라벨 위치
+        const labelY = arc.y + Math.sin(angle) * (radius + 8);
+
+        const borderColor = Array.isArray(dataset.borderColor)
+          ? (dataset.borderColor[index] as string)
+          : (dataset.borderColor as string);
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(labelX, labelY);
+        ctx.strokeStyle = borderColor || '#999';
+        ctx.lineWidth = 1;
+        ctx.stroke();
+      }
+    });
+  },
+});
