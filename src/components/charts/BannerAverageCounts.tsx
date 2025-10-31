10faefbfdb8f50a7582e11d9a13f5d98da7f8ade
alwjrqns <yyb02006@gmail.com>
2025-10-31 20:04:36 +0900

    Feat : hoverBackgroundColor 수정 및 쓰이지 않는 툴팁 컴포넌트 제거

--- src/components/charts/BannerAverageCounts.tsx
+++ src/components/charts/BannerAverageCounts.tsx
@@ -4,26 +4,6 @@ import ChartWrapper from '#/components/charts/ChartWrapper';
 import BrushBarChart from '#/components/charts/SummaryBrushBarChart';
 import { GachaSimulationMergedResult } from '#/components/PickupList';
 import { truncateToDecimals } from '#/libs/utils';
-import { TooltipContentProps } from 'recharts';
-
-const BannerGachaCountTooltip: React.FC<
-  TooltipContentProps<number, string> & { totalSimulationTry: number }
-> = ({ active, payload, label }) => {
-  if (!active || !payload || payload.length === 0) return null;
-  return (
-    <div className="space-y-3 rounded-lg bg-[#202020] px-4 py-3 shadow-lg shadow-[#202020]">
-      <p className="text-lg">{label}</p>
-      {payload.map((data, index) => (
-        <div key={index} className="font-S-CoreDream-400 space-y-[2px] text-sm">
-          <p>
-            {`평균 가챠횟수 : `}
-            <span className="text-amber-400">{`${data.value} 회`}</span>
-          </p>
-        </div>
-      ))}
-    </div>
-  );
-};
 
 export default function BannerAverageCounts({
   result,
@@ -47,7 +27,7 @@ export default function BannerAverageCounts({
           colors={{
             backgroundColor: '#fe9a00CC',
             borderColor: '#fe9a00',
-            hoverBackgroundColor: '#a684ff',
+            hoverBackgroundColor: '#a684ffCC',
             hoverBorderColor: '#a684ff',
           }}
           tooltipCallback={() => 'test'}
