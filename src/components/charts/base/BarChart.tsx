7dcb21a8fd3c4e063aab8443ebb44061730422a2
alwjrqns <yyb02006@gmail.com>
2025-11-02 07:14:52 +0900

    Feat : 커스텀 툴팁 추가

--- src/components/charts/base/BarChart.tsx
+++ src/components/charts/base/BarChart.tsx
@@ -55,6 +55,7 @@ export default function BarChart({
 }: BarChartProps) {
   const categoryPercentage = data.length < 50 ? 0.7 : data.length < 150 ? 0.8 : 0.9;
   const chartRef = useRef<ChartJS<'bar'>>(null);
+  const lastChartId = useRef<string | null>(null);
 
   const chartData: ChartData<'bar'> = {
     labels,
@@ -94,9 +95,62 @@ export default function BarChart({
     plugins: {
       legend: { display: false },
       tooltip: {
-        enabled: true,
+        enabled: false,
         mode: 'index', // x축 "열(column)" 단위로 hover 인식
         intersect: false, // 바 위가 아니라, 그 열 전체 hover 가능
+        external: (context) => {
+          const { chart, tooltip } = context;
+          const chartId = chart.canvas.id;
+
+          let tooltipEl = document.getElementById('custom-tooltip') as HTMLDivElement;
+          if (!tooltipEl) {
+            tooltipEl = document.createElement('div');
+            tooltipEl.id = 'custom-tooltip';
+            tooltipEl.className = 'absolute z-50 pointer-events-none';
+            document.body.appendChild(tooltipEl);
+          }
+
+          // Tooltip 숨김 처리
+          if (tooltip.opacity === 0 || !tooltip.body) {
+            tooltipEl.style.opacity = '0';
+            lastChartId.current = null;
+            return;
+          }
+
+          const rect = chart.canvas.getBoundingClientRect();
+          const x = tooltip.caretX ?? rect.width / 2;
+          const y = tooltip.caretY ?? rect.height / 2;
+
+          const sameChart = lastChartId.current === chartId;
+
+          // 차트 이동 감지
+          tooltipEl.style.transition = sameChart ? 'all 0.1s ease' : 'none';
+          tooltipEl.style.left = rect.left + window.scrollX + x + 'px';
+          tooltipEl.style.top = rect.top + window.scrollY + y + 'px';
+          tooltipEl.style.opacity = '1';
+
+          lastChartId.current = chartId;
+
+          // 내용 업데이트
+          const title = tooltip.title || [];
+          const body = tooltip.body;
+
+          const data = tooltip.dataPoints?.[0];
+          const textColor = data.dataset.borderColor;
+
+          const total = (data.dataset.data as number[]).reduce((a, b) => a + b, 0);
+
+          tooltipEl.innerHTML = `
+            <div class="space-y-3 rounded-xl bg-[#202020] px-4 py-3 shadow-xl shadow-[#141414]">
+              ${title.map((t) => `<p style="color: ${textColor}" class="text-lg font-S-CoreDream-500">${t}번째 가챠</p>`).join('')}
+              ${body
+                .map((b, i) => {
+                  return tooltipCallback(data, total ?? 1);
+                })
+                .join('')}
+            </div>
+          `;
+        },
       },
       datalabels: { display: false },
     },
