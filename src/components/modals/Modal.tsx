4fdb7b6502de89300fc28c3486e34714722cb4bf
alwjrqns <yyb02006@gmail.com>
2025-11-03 06:51:29 +0900

    Feat : onClick 이슈를 MouseUp, Down 이벤트로 수정 및 모달 포커싱 제한

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import { AnimatePresence, motion } from 'motion/react';
 import { createPortal } from 'react-dom';
 
@@ -12,10 +12,13 @@ interface ModalProps {
 
 export default function Modal({ children, isOpen, onClose }: ModalProps) {
   const [mounted, setMounted] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+  const isMouseDownOnTarget = useRef<boolean>(false);
 
   useEffect(() => {
     setMounted(true);
     if (isOpen) {
+      modalRef.current?.focus();
       document.body.style.overflow = 'hidden';
       document.documentElement.style.overflow = 'hidden';
     } else {
@@ -42,18 +45,28 @@ export default function Modal({ children, isOpen, onClose }: ModalProps) {
             className="fixed top-0 left-0 flex h-screen w-screen justify-center bg-[#00000090]"
           >
             <div
-              onClick={(e) => {
+              // tabIndex = -1의 의미는 포커스 가능한 요소이나 탭 순서 안에 안에 포함시키지는 않는다는 것
+              // div는 원래 포커스 가능한 요소가 아니기 때문에 tabIndex = -1을 주어야 focus() 메서드가 정상 작동함
+              ref={modalRef}
+              tabIndex={-1}
+              onMouseDown={(e) => {
                 if (e.target === e.currentTarget) {
+                  isMouseDownOnTarget.current = true;
+                } else {
+                  isMouseDownOnTarget.current = false;
+                }
+              }}
+              onMouseUp={(e) => {
+                if (e.target === e.currentTarget && isMouseDownOnTarget.current) {
                   onClose();
                 }
               }}
               onKeyDown={(e) => {
-                if (e.key === 'Enter' || e.key === ' ') {
+                if (e.key === 'Enter' || e.key === 'Escape' || e.key === ' ') {
                   onClose();
                 }
               }}
               role="button"
-              tabIndex={0}
               className="flex min-h-screen w-screen justify-center overflow-y-auto p-12"
             >
               <div className="my-auto">{children}</div>
