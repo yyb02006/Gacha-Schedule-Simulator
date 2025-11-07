a505703506cc207e231bbe9a4fd857fbcde96ae4
alwjrqns <yyb02006@gmail.com>
2025-11-08 07:47:05 +0900

    Feat : 배너별 진입 시 평균 재화 차트 추가

---
+++ src/components/charts/BannerBannerPreEVSuccess.tsx
@@ -0,0 +1,141 @@
+'use client';
+
+import ChartWrapper from '#/components/charts/base/ChartWrapper';
+import { GachaSimulationMergedResult } from '#/components/PickupList';
+import { ChartType, TooltipItem } from 'chart.js';
+import { truncateToDecimals } from '#/libs/utils';
+import BrushBarChart from '#/components/charts/base/BrushBarChart';
+
+export interface CreateTooltipLiteralProps<T extends ChartType> {
+  title: string[];
+  textColor: string;
+  body: {
+    before: string[];
+    lines: string[];
+    after: string[];
+  }[];
+  data: TooltipItem<T>;
+  total: number;
+}
+
+export type CreateTooltipLiteral<T extends ChartType> = ({
+  title,
+  textColor,
+  body,
+  data,
+  total,
+}: CreateTooltipLiteralProps<T>) => string;
+
+const createTooltipLiteral =
+  (result: GachaSimulationMergedResult) =>
+  ({ title, textColor, body, data, total }: CreateTooltipLiteralProps<'bar'>) => {
+    const parsedRawValue = typeof data.parsed.y === 'number' ? data.parsed.y : total;
+
+    const { bannerWinGachaRuns, bannerSuccess } = result.perBanner[data.dataIndex];
+
+    return /*html*/ `
+    <div class="space-y-3 rounded-xl bg-[#202020] opacity-90 px-4 py-3 shadow-xl shadow-[#141414]">
+    ${title.map((t) => `<p style="color: ${textColor}" class="text-lg font-S-CoreDream-500">${t}</p>`).join('')}
+    ${body
+      .map((b, i) => {
+        return /*html*/ `<div key={i} class="font-S-CoreDream-300 space-y-[2px] text-sm whitespace-nowrap">
+            <p>
+              기대값 도달 전 성공률 :
+              <span style="color: ${textColor};" class="font-S-CoreDream-500">
+                ${truncateToDecimals((parsedRawValue / total) * 100)}%
+              </span>
+            </p>
+            <p>
+              배너 성공률 : <span style="color: ${textColor};" class="font-S-CoreDream-500">${truncateToDecimals((bannerSuccess / total) * 100, 2)}%</span>
+            </p>
+            <p>
+              성공 시 기대값 : <span style="color: ${textColor};" class="font-S-CoreDream-500">${truncateToDecimals(bannerWinGachaRuns / bannerSuccess, 2)} 회</span>
+            </p>
+          </div>`;
+      })
+      .join('')}
+    </div>`;
+  };
+
+const DataProcessor = ({
+  result,
+  chartHeight,
+  brushHeight,
+  enableBrush = true,
+}: {
+  result: GachaSimulationMergedResult;
+  chartHeight?: string;
+  brushHeight?: string;
+  enableBrush?: boolean;
+}) => {
+  /* // 보정계수 1 (계산된 신뢰도에 보정을 위해 곱해줄 0~1사이의 값)
+  // baselineTrialCount을 기준으로 0.9이며 실제 시도횟수가 작아질수록 더 급속히 낮아짐
+  const baselineTrialCount = 10000; // 기준 시도횟수
+  const k = -Math.log(1 - 0.9) / baselineTrialCount; // 기준 시도횟수에서 보정도 90%가 나오는 k값
+  const correction = 1 - Math.exp(-k * result.total.simulationTry); // 실제 시도횟수 기반으로 사용할 보정계수. 시도횟수가 적을수록 보정도가 낮아져서 총 신뢰도가 작아짐 */
+
+  const data = result.perBanner.map(({ bannerWinGachaRuns, bannerSuccess, bannerHistogram }) => {
+    const expectedValueIndex = Math.floor(bannerWinGachaRuns / bannerSuccess);
+
+    const cumulativeToEVIndex = bannerHistogram
+      .slice(0, expectedValueIndex + 1)
+      .reduce((a, b) => a + b, 0);
+
+    return cumulativeToEVIndex;
+  });
+
+  return (
+    <BrushBarChart
+      labels={result.perBanner.map(({ name }) => name)}
+      data={data}
+      barChartColors={{
+        backgroundColor: '#fe9a00CC',
+        borderColor: '#fe9a00',
+        hoverBackgroundColor: '#8e51ffCC',
+        hoverBorderColor: '#8e51ff',
+      }}
+      brushColor={{
+        backgroundColor: '#8e51ffCC',
+        borderColor: '#8e51ff',
+      }}
+      total={result.total.simulationTry}
+      padding={16}
+      enableBrush={enableBrush}
+      isPercentYAxis={true}
+      chartHeight={chartHeight}
+      brushHeight={brushHeight}
+      tooltipCallback={createTooltipLiteral(result)}
+    />
+  );
+};
+
+export default function BannerPreEVSuccess({
+  result,
+  chartHeight,
+  brushHeight,
+  enableBrush = true,
+}: {
+  result: GachaSimulationMergedResult | null;
+  chartHeight?: string;
+  brushHeight?: string;
+  enableBrush?: boolean;
+}) {
+  return (
+    <ChartWrapper
+      title={
+        <span>
+          배너별 <span className="text-amber-400">기대값 도달 전 성공 확률</span>
+        </span>
+      }
+    >
+      {result ? (
+        <DataProcessor
+          result={result}
+          chartHeight={chartHeight}
+          brushHeight={brushHeight}
+          enableBrush={enableBrush}
+        />
+      ) : null}
+    </ChartWrapper>
+  );
+}
