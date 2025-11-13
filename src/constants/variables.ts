c6e41c0d56ab0e06bdbb81937266bffb3c6947ff
alwjrqns <yyb02006@gmail.com>
2025-11-13 22:46:39 +0900

    Feat : 목표 오퍼레이터 수 제한 값 상수 추가

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -1,3 +1,5 @@
+import { GachaType, OperatorRarityForString } from '#/types/types';
+
 export const rarityStrings = ['sixth', 'fifth', 'fourth'] as const;
 export const obtainedTypes = ['totalObtained', 'pickupObtained', 'targetObtained'] as const;
 export const rarities = {
@@ -13,3 +15,14 @@ export const obtainedKoreans = {
   pickupObtained: '픽업오퍼',
   targetObtained: '목표오퍼',
 } as const;
+export const operatorLimitByBannerType: Record<
+  GachaType,
+  Record<OperatorRarityForString, number>
+> = {
+  rotation: { sixth: 2, fifth: 3, fourth: 0 },
+  single: { sixth: 1, fifth: 2, fourth: 1 },
+  limited: { sixth: 2, fifth: 1, fourth: 0 },
+  collab: { sixth: 2, fifth: 2, fourth: 0 },
+  orient: { sixth: 3, fifth: 3, fourth: 0 },
+  contract: { sixth: 4, fifth: 6, fourth: 0 },
+};
