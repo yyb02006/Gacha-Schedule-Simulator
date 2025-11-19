e399e04529a8667299639eb33ee3394c10d91fe3
alwjrqns <yyb02006@gmail.com>
2025-11-20 00:46:45 +0900

    Feat : 클라우드플레어로부터 데이터 요철 로직 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,28 @@
 import GachaSystemInfoModal from '#/components/modals/GachaSystemInfoModal';
-import PickupList from '#/components/PickupList';
+import PickupList, { Dummy } from '#/components/PickupList';
+import { PickupDataPresets } from '#/types/types';
 
-export default function Home() {
+export default async function Home() {
+  const pickupDataPresets: { datas: PickupDataPresets } = await (
+    await fetch('https://pub-cee3b616ec754cb4b3678740fdae72a5.r2.dev/banners/pickupDatas.json')
+  ).json();
+
+  const now = Date.now();
+
+  const filteredPickupDataPresets = pickupDataPresets.datas.reduce<Dummy[]>((acc, current) => {
+    const { deactivationDate, ...rest } = current;
+    const parsedDeactivationDate = deactivationDate
+      ? new Date(deactivationDate).getTime()
+      : Infinity;
+    if (now < parsedDeactivationDate) {
+      acc.push(rest);
+    }
+    return acc;
+  }, []);
+
   return (
     <section className="relative flex w-screen justify-center">
-      <PickupList />
+      <PickupList pickupDataPresets={filteredPickupDataPresets} />
       <GachaSystemInfoModal />
     </section>
   );
