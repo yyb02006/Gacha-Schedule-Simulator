e24fa44ffd8a0fab2ec4542032f0c310e62e302e
alwjrqns <yyb02006@gmail.com>
2025-11-19 13:27:17 +0900

    Feat : 진행률에 따라 라인 색 변환 애니메이션 추가

--- src/components/GridCellBackground.tsx
+++ src/components/GridCellBackground.tsx
@@ -1,15 +1,49 @@
 import { motion } from 'motion/react';
+import { RefObject, useEffect, useRef, useState } from 'react';
 
 export default function GridCellBackground({
   cellSize = 120,
   divisions = 5,
   strokeColor = '#808080',
+  activeColor = '#bbb77b',
+  progressRef,
 }: {
   cellSize?: number;
   divisions?: number;
   strokeColor?: string;
+  activeColor?: string;
+  progressRef: RefObject<{
+    progressTry: number;
+    total: number;
+    gachaRuns: number;
+    success: number;
+  }>;
 }) {
   const step = cellSize / (divisions - 1); // 각 간격
+  const [progress, setProgress] = useState(0);
+  const lastUpdateRef = useRef(0);
+  const throttleMs = 100;
+
+  useEffect(() => {
+    let rafId: number;
+    function updateLineWithRAF() {
+      rafId = requestAnimationFrame(() => {
+        const now = performance.now();
+        if (now - lastUpdateRef.current >= throttleMs) {
+          lastUpdateRef.current = now;
+          if (progressRef && progressRef.current) {
+            const ratio = Math.min(progressRef.current.progressTry / progressRef.current.total, 1);
+            setProgress(ratio);
+          }
+        }
+        updateLineWithRAF();
+      });
+    }
+
+    updateLineWithRAF();
+    return () => cancelAnimationFrame(rafId);
+  }, [progressRef]);
+
   return (
     <svg width="100%" height="100%" className="absolute top-0 left-0">
       <defs>
@@ -17,7 +51,6 @@ export default function GridCellBackground({
           {/* 세로선 */}
           {Array.from({ length: divisions }).map((_, i) => {
             const x = i * step;
-
             return (
               <motion.line
                 key={`v${i}`}
@@ -25,14 +58,16 @@ export default function GridCellBackground({
                 y1={0}
                 x2={x}
                 y2={cellSize}
-                stroke={strokeColor}
                 strokeWidth="1"
                 strokeDasharray={cellSize}
-                strokeDashoffset={0}
-                initial={{ strokeDashoffset: cellSize }}
-                animate={{ strokeDashoffset: 0 }}
+                initial={{ strokeDashoffset: cellSize, stroke: strokeColor }}
+                animate={{
+                  strokeDashoffset: 0,
+                  stroke: progress * 1.1 * (divisions - 1) >= i ? activeColor : strokeColor,
+                  transition: { stroke: { duration: 0.5 } },
+                }}
                 exit={{ strokeDashoffset: -cellSize }}
-                transition={{ delay: i * 0.1, duration: 0.5 }}
+                transition={{ delay: i * 0.1 + 0.3, duration: 0.5 }}
               />
             );
           })}
@@ -40,7 +75,6 @@ export default function GridCellBackground({
           {/* 가로선 */}
           {Array.from({ length: divisions }).map((_, i) => {
             const y = i * step;
-
             return (
               <motion.line
                 key={`h${i}`}
@@ -48,14 +82,16 @@ export default function GridCellBackground({
                 y1={y}
                 x2={cellSize}
                 y2={y}
-                stroke={strokeColor}
                 strokeWidth="1"
                 strokeDasharray={cellSize}
-                strokeDashoffset={0}
-                initial={{ strokeDashoffset: cellSize }}
-                animate={{ strokeDashoffset: 0 }}
+                initial={{ strokeDashoffset: cellSize, stroke: strokeColor }}
+                animate={{
+                  strokeDashoffset: 0,
+                  stroke: progress * 1.1 * (divisions - 1) >= i ? activeColor : strokeColor,
+                  transition: { stroke: { duration: 0.5 } },
+                }}
                 exit={{ strokeDashoffset: -cellSize }}
-                transition={{ delay: i * 0.15, duration: 0.5 }}
+                transition={{ delay: i * 0.1 + 0.3, duration: 0.5 }}
               />
             );
           })}
