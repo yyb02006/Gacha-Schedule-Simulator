be894d7f15bfccc7fd1059791e3fcb1c1282ef93
alwjrqns <yyb02006@gmail.com>
2025-11-24 07:28:27 +0900

    Feat : fetch no-store 옵션 추가로 캐시 무효화

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,9 @@ import PickupList, { Dummy } from '#/components/PickupList';
 
 export default async function Home() {
   const pickupDataPresets: { datas: Dummy[] } = await (
-    await fetch('https://pub-cee3b616ec754cb4b3678740fdae72a5.r2.dev/banners/pickupDatas.json')
+    await fetch('https://pub-cee3b616ec754cb4b3678740fdae72a5.r2.dev/banners/pickupDatas.json', {
+      cache: 'no-store',
+    })
   ).json();
 
   const now = Date.now();
